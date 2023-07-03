046c2b653a9d95f45dda45988c7cb5a1c698d7e8
Yiyang Wu <toolmanp@outlook.com>
2023-07-03 14:43:26 +0800

    feat(verify): verification server impl
    Feat:
    - All internal managers hooks
    - Request Json Binding
    - Handler Implementation for Sign, Verify
    and Sign primitives.
    
    Signed-off-by: Yiyang Wu <toolmanp@outlook.com>

--- internal/verify/server.go
+++ internal/verify/server.go
@@ -1,8 +1,10 @@
 package verify
 
 import (
+	"encoding/hex"
 	"fmt"
-	"net/http"
+	"io"
+	"strconv"
 
 	"github.com/Cealgull/Verify/internal/email"
 	"github.com/Cealgull/Verify/internal/fabric"
@@ -22,28 +24,101 @@ type VerificationServer struct {
 	ts   *turnstile.Turnstile
 }
 
+type EmailRequest struct {
+	Account string `json:"account"`
+	Code    int    `json:"code,omitempty"`
+}
+
+type CodeMessage struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+type RegisterRequest struct {
+	Id     string `json:"id"`
+	Secret string `json:"secret"`
+}
+
 func New(host string, port string, em *email.EmailManager, fm *fabric.FabricManager, sm *sign.SignManager, ts *turnstile.Turnstile) *VerificationServer {
 
 	ec := echo.New()
 	v := VerificationServer{host, port, ec, em, fm, sm, ts}
 	v.ec.Use(middleware.Logger())
 	v.ec.Use(middleware.Recover())
-	v.ec.GET("/verify", v.verify)
-	v.ec.GET("/register", v.register)
-	v.ec.GET("/sign", v.sign)
+	v.ec.POST("/auth/verify", v.verify)
+	v.ec.POST("/auth/register", v.register)
+	v.ec.POST("/auth/sign", v.sign)
 	return &v
 }
 
 func (v *VerificationServer) verify(c echo.Context) error {
-	return c.String(http.StatusOK, "this is a verifcation route for email code, sending signed auth token")
+	var req EmailRequest
+
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(500, CodeMessage{500, err.Error()})
+	}
+	success, err := v.em.Verify(req.Account, strconv.Itoa(req.Code))
+	if err != nil {
+		return c.JSON(500, CodeMessage{500, err.Error()})
+	}
+	if success {
+		return c.JSON(200, v.sm.Dispatch())
+	} else {
+		return c.JSON(404, CodeMessage{
+			404,
+			"Err: Verification Failed for current account",
+		})
+	}
 }
 
 func (v *VerificationServer) register(c echo.Context) error {
-	return c.String(http.StatusOK, "verify auth token in zero knowledge membership proof style and ask fabric to enroll the identity")
+	var req RegisterRequest
+
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(500, CodeMessage{500, err.Error()})
+	}
+
+	signature, err := hex.DecodeString(c.Request().Header.Get("Signature"))
+
+	if err != nil || signature == nil {
+		return c.JSON(500, CodeMessage{500, "Err: Signature not found in headers"})
+	}
+
+	body, err := io.ReadAll(c.Request().Body)
+
+	if err != nil {
+		return c.JSON(500, CodeMessage{500, err.Error()})
+	}
+
+	success, err := v.sm.Verify(body, []byte(signature))
+
+	if err != nil {
+		return c.JSON(500, CodeMessage{500, err.Error()})
+	}
+
+	if success {
+		return c.JSON(200, CodeMessage{200, "OK"})
+	} else {
+		return c.JSON(500, CodeMessage{500, "Err: Signature not matching the content"})
+	}
+
 }
 
-func (s *VerificationServer) sign(c echo.Context) error {
-	return c.String(http.StatusOK, "generate an authorization code and invoke email service api, send the code to the requester")
+func (v *VerificationServer) sign(c echo.Context) error {
+
+	var req EmailRequest
+
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(500, CodeMessage{500, err.Error()})
+	}
+	_, err := v.em.Sign(req.Account)
+
+	if err != nil {
+		return c.JSON(500, CodeMessage{500, err.Error()})
+	}
+
+	return c.JSON(200, CodeMessage{200, "OK"})
+
 }
 
 func (s *VerificationServer) Start() {
