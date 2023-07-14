b3c5871899ca787b5b9e149c3b51160d348ebea6
Yiyang Wu <toolmanp@outlook.com>
2023-07-14 10:44:03 +0800

    test(cert): update cert manager unittests
    Signed-off-by: Yiyang Wu <toolmanp@outlook.com>

--- internal/cert/manager_test.go
+++ internal/cert/manager_test.go
@@ -1,1 +1,21 @@
 package cert
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadPem(t *testing.T) {
+	b, err := loadPemFromDisk("./notexists", CERT)
+	assert.Nil(t, b)
+	assert.Error(t, err)
+
+	b, err = loadPemFromDisk("./testdata/priv.pem", PRIVATE)
+	assert.NotNil(t, b)
+	assert.NoError(t, err)
+
+	b, err = loadPemFromDisk("./testdata/priv.pem", CERT)
+	assert.Nil(t, b)
+	assert.Error(t, err)
+}
