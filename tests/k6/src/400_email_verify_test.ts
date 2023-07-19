48cad957c3fc36293ff327aa13cae6509b349e26
Yiyang Wu <toolmanp@outlook.com>
2023-07-19 23:11:24 +0800

    test(email): update verify load test
    Signed-off-by: Yiyang Wu <toolmanp@outlook.com>

---
+++ tests/k6/src/400_email_verify_test.ts
@@ -0,0 +1,14 @@
+import http from "k6/http";
+
+export default function() {
+  const number = Math.floor(Math.random() * 1000000);
+  const payload = JSON.stringify({
+    account: "user" + number.toString(),
+    code: number.toString().padStart(6, "0"),
+  });
+  const headers = { "content-type": "application/json"};
+  http.post("http://localhost:8080/email/verify",
+    payload,
+    {headers}
+  );
+}
